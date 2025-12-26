d3ac2a0d7ee85b56f417f990b6a53e5c2e8ef7cd
piyush472 <piyushnegi310@gmail.com>
2025-12-26 12:10:06 +0530

    new programs added

---
+++ FUNCTIONS/01fun.py
@@ -0,0 +1,11 @@
+"""
+Program: Write a function that takes a number and returns its square.
+Topic: Functions
+Author: Piyush Negi
+"""
+
+
+
+def square(a):
+    return a*a
+print(square(3))
--- FUNCTIONS/1fun.py
+++
@@ -1,4 +0,0 @@
-#Write a function that takes a number and returns its square.
-def square(a):
-    return a*a
-print(square(3))
